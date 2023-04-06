47ca738ec72711c1cc903ba789839d8e6d5d381a
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 05:50:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Variables
+#all varibales must start with lowercase or underscore and only can have letters, numbers, and underscores
+
+iq = 190 #anything can be named as a variable
+catTail = 67
+snake_caste = 12
+doood = iq/4 #you can use variables in equations
+
+print(iq)
+print(catTail)
+print(snake_caste)
+print(doood)
+
+
+a,b,c = 1,2,3
+
+print(a)
+print(b)
+print(c)
+print(c,a,b)
+print(a,b,c)
+print(b,c,a)
+
+#Constants = things that aren't meant to change in program
+PI = 3.14
